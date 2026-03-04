4b117c3df94504ea1342351b0d5d67ba8a1a1df7
teresaov <teresa.orbillo@mail.utoronto.ca>
2026-03-03 21:16:07 -0500

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -40,12 +40,10 @@ mkdir data/processed/server_logs
 mkdir data/processed/user_logs
 mkdir data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cd data/raw
-cp server*.log data/processed/server_logs
+cp ./data/raw server*.log ./data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-cd data/raw
-cp user*.log data/processed/user_logs
-cp event*.log data/processed/event_logs
+cp ./data/raw user*.log ./data/processed/user_logs
+cp ./data/ event*.log ./data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 cd data/raw
 rm user_ipaddr*.log
@@ -54,7 +52,7 @@ cd data/processed
 cd data/processed/user_logs
 rm user_ipaddr*.log
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-nano data/inventory.txt 
+echo "server_logs user_logs event_logs" data/inventory.txt 
 
 ###########################################
 
